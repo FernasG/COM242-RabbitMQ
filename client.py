babfe357fd8e063e2209a76c635a1ff0dbee36a0
FernasG <fernandogoul7@gmail.com>
2023-09-10 14:28:00 -0300

    feat: finish server and client logic

--- client.py
+++ client.py
@@ -1,13 +1,88 @@
-from pika import BlockingConnection, ConnectionParameters
+import os, ast, uuid
 
-if __name__ == "__main__": 
-    queues = ["hello"]
-    connection = BlockingConnection(ConnectionParameters("localhost", "5672"))
-    channel = connection.channel()
+from pika import BlockingConnection, ConnectionParameters, BasicProperties
+from pika.adapters.blocking_connection import BlockingChannel
+from pika.spec import Basic
 
-    for queue in queues:
-        channel.queue_declare(queue)
+class Client:
+    def __init__(self) -> None:
+        queues = ["text_message", "file_edit", "calc"]
+        self.connection = BlockingConnection(ConnectionParameters("localhost", "5672"))
+        self.channel = self.connection.channel()
+
+        for queue in queues:
+            self.channel.queue_declare(queue)
+
+        queue_result = self.channel.queue_declare(queue="", exclusive=True)
+        self.reply_queue = queue_result.method.queue
+
+        self.channel.basic_consume(queue=self.reply_queue, on_message_callback=self.on_response, auto_ack=True)
+
+        self.response = None
+        self.correlation_id = None
     
-    channel.basic_publish(exchange='', routing_key='hello', body='Hello World!')
-    print("[x] Sent 'Hello World!'")
-    connection.close()
+    def on_response(self, channel: BlockingChannel, method: Basic.Deliver, properties: BasicProperties, body: bytes):
+        if self.correlation_id == properties.correlation_id:
+            self.response = body.decode()
+
+    def text_message(self) -> dict:
+        text = input("Escreva a sua mensagem: ")
+        body = str({"text": text})
+        return {"routing_key": "text_message", "body": body}
+
+    def file_edit(self) -> dict:
+        text = input("Escreva o texto: ")
+        body = str({"text": text})
+        return {"routing_key": "file_edit", "body": body}
+
+    def calc(self) -> dict:
+        x = int(input("Digite um número: "))
+        y = int(input("Digite outro número: "))
+        body = str({"x": x, "y": y})
+        return {"routing_key": "calc", "body": body}
+    
+    def call(self, option: int):
+        options_map = {"1": self.text_message, "2": self.file_edit, "3": self.calc}
+
+        function = options_map.get(str(option), None)
+        params = function()
+
+        self.response = None
+        self.correlation_id = str(uuid.uuid4())
+
+        self.channel.basic_publish(exchange="", properties=BasicProperties(reply_to=self.reply_queue, correlation_id=self.correlation_id), **params)
+        self.connection.process_data_events(time_limit=None)
+        return self.response
+
+def menu() -> str:
+    print("*=" * 20)
+    print("Menu")
+    print("1 - Responder uma mensagem")
+    print("2 - Editar arquivo de text")
+    print("3 - Calculo de uma função")
+    print("9 - Sair")
+
+    return input("Escolha uma das opções acima: ")
+
+
+if __name__ == "__main__":
+    client = Client()
+
+    loop = True
+
+    while loop:
+        # os.system("clear")
+        option = menu()
+
+        if not option.isdigit(): continue
+
+        option = int(option)
+
+        if option not in [1, 2, 3]:
+            print("Saindo...")
+            loop = False
+        
+        response = client.call(option)
+        print(response)
+
+    # connection.close()
--- server.py
+++ server.py
@@ -1,17 +1,51 @@
-from pika import BlockingConnection, ConnectionParameters
+import ast
 
-def hello(ch, method, properties, body):
-    print(f"[x] Received {body}")
+from pika import BlockingConnection, ConnectionParameters, BasicProperties
+from pika.spec import Basic
+from pika.adapters.blocking_connection import BlockingChannel
+
+
+def text_message(channel: BlockingChannel, method: Basic.Deliver, properties: BasicProperties, body: bytes) -> None:
+    body = ast.literal_eval(body.decode())
+    text = body.get("text", "")
+    message = f"Olá, você digitou o texto: \"{text}\""
+
+    channel.basic_publish(exchange="", routing_key=properties.reply_to, properties=BasicProperties(correlation_id=properties.correlation_id), body=message)
+    channel.basic_ack(delivery_tag=method.delivery_tag)
+
+def file_edit(channel: BlockingChannel, method: Basic.Deliver, properties: BasicProperties, body: bytes) -> None:
+    body = ast.literal_eval(body.decode())
+    text = body.get("text", "")
+
+    with open("text", "w") as file:
+        file.write(text)
+
+    message = f"Você escreveu \"{text}\" no arquivo text"
+    channel.basic_publish(exchange="", routing_key=properties.reply_to, properties=BasicProperties(correlation_id=properties.correlation_id), body=message)
+    channel.basic_ack(delivery_tag=method.delivery_tag)
+
+def calc(channel: BlockingChannel, method: Basic.Deliver, properties: BasicProperties, body: bytes) -> None:
+    body = ast.literal_eval(body.decode())
+    x = body.get("x", 0)
+    y = body.get("y", 0)
+
+    message = f"{x} + {y} = {x+y}\n{x} - {y} = {x-y}\n{x} / {y} = {x/y}\n{x} * {y} = {x*y}"
+
+    channel.basic_publish(exchange="", routing_key=properties.reply_to, properties=BasicProperties(correlation_id=properties.correlation_id), body=message)
+    channel.basic_ack(delivery_tag=method.delivery_tag)
 
 if __name__ == "__main__": 
-    queues = ["hello"]
-    connection = BlockingConnection(ConnectionParameters("rabbitmq", "5672"))
+    queues = ["text_message", "file_edit", "calc"]
+    connection = BlockingConnection(ConnectionParameters("localhost", "5672"))
     channel = connection.channel()
+    channel.basic_qos(prefetch_count=1)
 
     for queue in queues:
         channel.queue_declare(queue)
     
-    channel.basic_consume(queue="hello", auto_ack=True, on_message_callback=hello)
+    channel.basic_consume(queue="text_message", on_message_callback=text_message)
+    channel.basic_consume(queue="file_edit", on_message_callback=file_edit)
+    channel.basic_consume(queue="calc", on_message_callback=calc)
 
     print("[*] Waiting for messages. To exit press CTRL+C")
     channel.start_consuming()
